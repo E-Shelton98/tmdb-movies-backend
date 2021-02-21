a068a8d808321b49c5df0c0700dc2cc5024dd665
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-21 03:10:03 -0500

    enabling trust proxy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = 3000
 
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
-// app.set('trust proxy', 1);
+app.set('trust proxy', 1);
 
 const limiter = rateLimit({
   windowMs: 1000, // 1 second
