ac8d01433256ac656973c81a530ac10971ecbc46
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-21 03:03:14 -0500

    first commit, majority of work completed, uploading for deployment and in case of future feature expandability

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
+_NOTE_ This is heavily modified from the tutorial at the clone link below. Though I have made some slight edits such as adding increased functionality, Much of the code was already there, I simply expanded or tweaked it to fit my needs as a refresher on Express.
+
 # Good Relay
 
 A simple example node backend that demonstrates how to hit a 3rd party API without exposing your API key in your frontend code.
-
-_Note_ the goodreads API returns XML which is tricky to work with. To see a more simple verison of this, checkout the `weather-json-example` branch. To use that branch you'll need a free API key from Open Weather Map. There is no front-end for it, so you need to interact with it via curl or Postman. The api url is `localhost:3000/onecall?lat={some latitidue}&lon={some longtitude}`.
 
 ## Usage
 
@@ -10,7 +10,7 @@ _Note_ the goodreads API returns XML which is tricky to work with. To see a more
 
 You need nodejs installed: https://nodejs.org/en/download/
 
-### Initialise the App
+### Initialize the App
 
 To get started git clone this repo (choose one depending on whether you prefer https or ssh - pick the first one if you're not sure):
 
@@ -62,17 +62,3 @@ Set Postman to send a Get request and paste this in the url: `localhost:3000/api
 Postman will show you the JSON response below.
 
 ## How do you use this in your front end?
-
-This simple app is listening for get requests at `/api/search`, so interact with it in your front end app the way you have been previously with the original api.
-
-This is only configured to handle search queries - if you want to use other Goodreads API endpoints / methods, you'll need to think about how you implement them yourself!
-
-## Goodreads search
-
-See more options for searching at [Goodreads docs](https://www.goodreads.com/api/index#search.books)
-
-## Hosting
-
-You can't deploy your frontend and still have this on localhost - obviously you need to deploy this, too.
-
-I recommend [Heroku](https://devcenter.heroku.com/articles/deploying-nodejs).
--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // These import necessary modules and set some initial variables
-require("dotenv").config();
-const express = require("express");
-const fetch = require("node-fetch");
-const convert = require("xml-js");
-const rateLimit = require("express-rate-limit");
-var cors = require("cors");
-const app = express();
-const port = 3000;
+require('dotenv').config()
+const express = require('express')
+const fetch = require('node-fetch')
+const rateLimit = require('express-rate-limit')
+var cors = require('cors')
+const app = express()
+const port = 3000
 
 // Rate limiting - Goodreads limits to 1/sec, so we should too
 
@@ -17,55 +16,72 @@ const port = 3000;
 const limiter = rateLimit({
   windowMs: 1000, // 1 second
   max: 1, // limit each IP to 1 requests per windowMs
-});
+})
 
 //  apply to all requests
-app.use(limiter);
+app.use(limiter)
 
 // Allow CORS from any origin
-app.use(cors());
+app.use(cors())
 
 // Routes
 
 // Test route, visit localhost:3000 to confirm it's working
 // should show 'Hello World!' in the browser
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'))
 
-// Our Goodreads relay route!
-app.get("/api/search", async (req, res) => {
+//TMDb discover route
+app.get('/api/tmdb/discover', async (req, res) => {
   try {
-    // This uses string interpolation to make our search query string
-    // it pulls the posted query param and reformats it for goodreads
-    const searchString = `q=${req.query.q}`;
+    // This uses string interpolation to make our type query (ie. movie/tv) string.
+    // It pulls the posted query param and reformats it for TMDb
+    const typeString = req.query.type
 
-    // It uses node-fetch to call the goodreads api, and reads the key from .env
+    // It uses node-fetch to call the TMDb api, and reads the key from .env
     const response = await fetch(
-      `https://www.goodreads.com/search/index.xml?key=${process.env.GOODREADS_API_KEY}&${searchString}`,
-    );
-    //more info here https://www.goodreads.com/api/index#search.books
-    const xml = await response.text();
-
-    // Goodreads API returns XML, so to use it easily on the front end, we can
-    // convert that to JSON:
-    const json = convert.xml2json(xml, { compact: true, spaces: 2 });
-
-    // The API returns stuff we don't care about, so we may as well strip out
-    // everything except the results:
-    const results = JSON.parse(json).GoodreadsResponse.search.results;
+      `https://api.themoviedb.org/3/discover/${typeString}?sort_by=popularity.desc&api_key=${process.env.TMDB_API_KEY}`
+    )
+    const data = await response.results.json()
 
     return res.json({
       success: true,
-      results,
-    });
+      data,
+    })
   } catch (err) {
     return res.status(500).json({
       success: false,
       message: err.message,
-    });
+    })
   }
-});
+})
 
-// This spins up our sever and generates logs for us to use.
-// Any console.log statements you use in node for debugging will show up in your
-// terminal, not in the browser console!
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//TMDb search route
+app.get('/api/tmdb/search', async (req, res) => {
+  try {
+    // This uses string interpolation to make our search query string
+    // it pulls the posted query param and reformats it for TMDb, same for type.
+    const typeString = req.query.type
+    const searchString = `query=${req.query.query}`
+
+    // It uses node-fetch to call the TMDb api, and reads the key from .env
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/${typeString}?api_key=${process.env.TMDB_API_KEY}&${searchString}`
+    )
+    const data = await response.json()
+    
+
+    return res.json({
+      success: true,
+      data,
+    })
+      
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    })
+  }
+})
+
+//spin up the server, console log to verify.
+app.listen(port, () => console.log(`Listening on Port ${port}!`))
